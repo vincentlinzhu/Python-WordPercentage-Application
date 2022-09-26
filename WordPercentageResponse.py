e96a6958626374406b45647553ada9091e68ba09
Vincent Zhu <vincentzhu@ucsb.edu>
2022-09-25 17:51:37 -0700

    SLight cleaning

--- WordPercentageResponse.py
+++ WordPercentageResponse.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
 import os
 import re
 import sys
